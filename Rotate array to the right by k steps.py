89529ba6818012cef7e180ef18074e660b4c270b
Barkavi Rajan <115571297+Barkavirajan@users.noreply.github.com>
2026-03-13 08:23:41 +0530

    updated file

--- Rotate array to the right by k steps.py
+++ Rotate array to the right by k steps.py
@@ -8,3 +8,21 @@ def rotate_array(nums, k):
 arr = [1,2,3,4,5,6,7]
 k = 3
 print(rotate_array(arr, k))
+
+#without using reverse
+
+def reverse(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+def rotate_array(arr, k):
+    n = len(arr)
+    k = k % n
+    reverse(arr, 0, n-1)
+    reverse(arr, 0, k-1)
+    reverse(arr, k, n-1)
+    return arr
+arr = [1,2,3,4,5,6,7]
+k = 3
+print(rotate_array(arr, k))
